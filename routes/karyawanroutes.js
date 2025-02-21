29ee88e858d535adccbef32da1e9b5b1bb317cf1
alessandrofhs <alessandrofhs@gmail.com>
2025-02-21 16:30:15 +0700

    push

--- routes/karyawanroutes.js
+++ routes/karyawanroutes.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const karyawanController = require("../controllers/karyawanController");
-const authMiddleware = require("../middlewares/auth");
+const karyawanController = require('../controllers/karyawanController');
+const authMiddleware = require('../middlewares/auth');
 
-router.get("/", authMiddleware, karyawanController.getAllKaryawan);
-router.get("/:id", authMiddleware, karyawanController.getKaryawanById);
-router.post("/", authMiddleware, karyawanController.createKaryawan);
-router.put("/:id", authMiddleware, karyawanController.updateKaryawan);
-router.delete("/:id", authMiddleware, karyawanController.deleteKaryawan);
+router.get('/', authMiddleware, karyawanController.getAllKaryawan);
+router.get('/:id', authMiddleware, karyawanController.getKaryawanById);
+router.post('/', authMiddleware, karyawanController.createKaryawan);
+router.put('/:id', authMiddleware, karyawanController.updateKaryawan);
+router.delete('/:id', authMiddleware, karyawanController.deleteKaryawan);
 
 module.exports = router;
